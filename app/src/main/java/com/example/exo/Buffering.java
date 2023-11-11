cd0c5e2526f249c53e1718ad9597cf5d62f6d9e5
ParhamVA <Parham.vali97@gmail.com>
2023-11-11 11:55:51 +0330

    Add Log to try catch Buffering

--- app/src/main/java/com/example/exo/Buffering.java
+++ app/src/main/java/com/example/exo/Buffering.java
@@ -1,5 +1,7 @@
 package com.example.exo;
 
+import android.util.Log;
+
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.LoadControl;
 
@@ -8,10 +10,12 @@ public class Buffering {
     public static LoadControl getBuffer(int buffer) {
         DefaultLoadControl loadControl;
         try {
+            Log.i("Buffering_Test","try");
             loadControl = new DefaultLoadControl.Builder()
                     .setBufferDurationsMs(400000, 400000, 2500, 5000)
                     .build();
         } catch (Exception e) {
+            Log.i("Buffering_Test","catch");
             loadControl = new DefaultLoadControl.Builder()
                     .setBufferDurationsMs(DefaultLoadControl.DEFAULT_MIN_BUFFER_MS, DefaultLoadControl.DEFAULT_MAX_BUFFER_MS,
                             DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS, DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS)
