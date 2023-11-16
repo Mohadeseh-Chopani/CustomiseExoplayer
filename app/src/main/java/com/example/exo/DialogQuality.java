ca0a27aa918e052360f9655c1fce753a2d382c53
mohadeseh chopani <chopanimohadeseh0@gmail.com>
2023-11-16 16:11:36 +0330

    Get automatic quality from video and set in dialog

--- app/src/main/java/com/example/exo/DialogQuality.java
+++ app/src/main/java/com/example/exo/DialogQuality.java
@@ -1,115 +1,397 @@
 package com.example.exo;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Pair;
 import android.util.SparseArray;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector.SelectionOverride;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector.MappedTrackInfo;
+import com.google.android.exoplayer2.ui.TrackNameProvider;
+import com.google.android.exoplayer2.ui.TrackSelectionView;
 import com.google.android.exoplayer2.util.Assertions;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class DialogQuality extends DialogFragment {
+/**
+ * Dialog to select tracks.
+ */
+public final class DialogQuality extends DialogFragment {
+    static DialogInterface dialogInterface;
+    private final SparseArray<TrackSelectionViewFragment> tabFragments;
+    private final ArrayList<Integer> tabTrackTypes;
+    static boolean ItemSelected = false;
+    private static DialogInterface.OnClickListener onClickListener;
+    private DialogInterface.OnDismissListener onDismissListener;
+    private int titleId;
 
-    ListenerQuality listenerQuality;
-    RadioGroup radio_Group;
-    RadioButton radiobtn_automatic, radiobtn_360, radiobtn_480, radiobtn_720, radiobtn_1080;
+    /**
+     * Returns whether a track selection dialog will have content to display if initialized with the
+     * specified {@link DefaultTrackSelector} in its current state.
+     */
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        listenerQuality = (ListenerQuality) context;
+    public static boolean willHaveContent(DefaultTrackSelector trackSelector) {
+        MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
+        return mappedTrackInfo != null && willHaveContent(mappedTrackInfo);
     }
 
-    int status;
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Window window = getDialog().getWindow();
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        if (window == null)
+            return;
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.width = 800;
+        params.height = 1200;
+        window.setAttributes(params);
+    }
+
+    /**
+     * Returns whether a track selection dialog will have content to display if initialized with the
+     * specified {@link MappedTrackInfo}.
+     */
+    public static boolean willHaveContent(MappedTrackInfo mappedTrackInfo) {
+        for (int i = 0; i < mappedTrackInfo.getRendererCount(); i++) {
+            if (showTabForRenderer(mappedTrackInfo, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Creates a dialog for a given {@link DefaultTrackSelector}, whose parameters will be
+     * automatically updated when tracks are selected.
+     *
+     * @param trackSelector     The {@link DefaultTrackSelector}.
+     * @param onDismissListener A {@link DialogInterface.OnDismissListener} to call when the dialog is
+     *                          dismissed.
+     */
+    public static DialogQuality createForTrackSelector(
+            DefaultTrackSelector trackSelector, DialogInterface.OnDismissListener onDismissListener) {
+        MappedTrackInfo mappedTrackInfo =
+                Assertions.checkNotNull(trackSelector.getCurrentMappedTrackInfo());
+
+
+        DialogQuality dialogQuality = new DialogQuality();
+        DefaultTrackSelector.Parameters parameters = trackSelector.getParameters();
+
+
+        dialogQuality.init(
+                /* titleId= */ R.string.select_quality,
+                mappedTrackInfo,
+                /* initialParameters = */ parameters,
+                /* allowAdaptiveSelections= */ false,
+                /* allowMultipleOverrides= */ false,
+                /* onClickListener= */ (dialog, which) -> {
+                    DefaultTrackSelector.ParametersBuilder builder = parameters.buildUpon();
+                    for (int i = 0; i < mappedTrackInfo.getRendererCount(); i++) {
+                        builder
+                                .clearSelectionOverrides(/* rendererIndex= */ i)
+                                .setRendererDisabled(
+                                        /* rendererIndex= */ i,
+                                        dialogQuality.getIsDisabled(/* rendererIndex= */ i));
+                        List<SelectionOverride> overrides =
+                                dialogQuality.getOverrides(/* rendererIndex= */ i);
+                        if (!overrides.isEmpty()) {
+                            builder.setSelectionOverride(
+                                    /* rendererIndex= */ i,
+                                    mappedTrackInfo.getTrackGroups(/* rendererIndex= */ i),
+                                    overrides.get(0));
+                        }
+                    }
+                    trackSelector.setParameters(builder);
+
+                },
+                onDismissListener);
+        return dialogQuality;
+    }
+
+    public DialogQuality() {
+        tabFragments = new SparseArray<>();
+        tabTrackTypes = new ArrayList<>();
+        // Retain instance across activity re-creation to prevent losing access to init data.
+        setRetainInstance(true);
+    }
+
+    private void init(
+            int titleId,
+            MappedTrackInfo mappedTrackInfo,
+            DefaultTrackSelector.Parameters initialParameters,
+            boolean allowAdaptiveSelections,
+            boolean allowMultipleOverrides,
+            DialogInterface.OnClickListener onClickListener,
+            DialogInterface.OnDismissListener onDismissListener) {
+        this.titleId = titleId;
+        this.onClickListener = onClickListener;
+        this.onDismissListener = onDismissListener;
+        for (int i = 0; i < mappedTrackInfo.getRendererCount(); i++) {
+            if (showTabForRenderer(mappedTrackInfo, i)) {
+                int trackType = mappedTrackInfo.getRendererType(/* rendererIndex= */ i);
+                TrackGroupArray trackGroupArray = mappedTrackInfo.getTrackGroups(i);
+                TrackSelectionViewFragment tabFragment = new TrackSelectionViewFragment();
+
+
+                tabFragment.init(
+                        mappedTrackInfo,
+                        /* rendererIndex= */ i,
+                        initialParameters.getRendererDisabled(/* rendererIndex= */ i),
+                        initialParameters.getSelectionOverride(/* rendererIndex= */ i, trackGroupArray),
+                        allowAdaptiveSelections,
+                        allowMultipleOverrides);
+                tabFragments.put(i, tabFragment);
+                tabTrackTypes.add(trackType);
+            }
+        }
+    }
+
+    /**
+     * Returns whether a renderer is disabled.
+     *
+     * @param rendererIndex Renderer index.
+     * @return Whether the renderer is disabled.
+     */
+    public boolean getIsDisabled(int rendererIndex) {
+        TrackSelectionViewFragment rendererView = tabFragments.get(rendererIndex);
+        return rendererView != null && rendererView.isDisabled;
+    }
+
+    /**
+     * Returns the list of selected track selection overrides for the specified renderer. There will
+     * be at most one override for each track group.
+     *
+     * @param rendererIndex Renderer index.
+     * @return The list of track selection overrides for this renderer.
+     */
+    public List<SelectionOverride> getOverrides(int rendererIndex) {
+        TrackSelectionViewFragment rendererView = tabFragments.get(rendererIndex);
+        return rendererView == null ? Collections.emptyList() : rendererView.overrides;
+    }
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // We need to own the view to let tab layout work correctly on all API levels. We can't use
+        // AlertDialog because it owns the view itself, so we use AppCompatDialog instead, themed using
+        // the AlertDialog theme overlay with force-enabled title.
+        AppCompatDialog dialog = new AppCompatDialog(getActivity(), R.style.MyCustomTheme);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.quality_dialog, null, false);
+        dialog.setContentView(view);
+        dialog.setTitle(R.string.select_quality);
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.background_dialog);
+        return dialog;
+    }
+
+    @Override
+    public void onDismiss(@NonNull DialogInterface dialog) {
+        super.onDismiss(dialog);
+        onDismissListener.onDismiss(dialog);
+    }
+
+    static View dialogView;
 
     @SuppressLint("MissingInflatedId")
-    @NonNull
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext(), R.style.MyCustomTheme);
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_quality, null, false);
-        builder.setView(view);
+    public View onCreateView(
+            LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        dialogView = inflater.inflate(R.layout.quality_dialog, container, false);
+        TabLayout tabLayout = dialogView.findViewById(R.id.track_selection_dialog_tab_layout);
+        ViewPager viewPager = dialogView.findViewById(R.id.track_selection_dialog_view_pager);
 
-        radio_Group = view.findViewById(R.id.radio_group_quality);
-        radiobtn_automatic = view.findViewById(R.id.quality_atomatic);
-        radiobtn_360 = view.findViewById(R.id.quality_360);
-        radiobtn_480 = view.findViewById(R.id.quality_480);
-        radiobtn_720 = view.findViewById(R.id.quality_720);
-        radiobtn_1080 = view.findViewById(R.id.quality_1080);
+        dialogInterface = getDialog();
 
-        switch (MainActivity.currentQuality) {
-            case 0:
-                radiobtn_automatic.setChecked(true);
-                break;
-            case 1:
-                radiobtn_360.setChecked(true);
-                break;
-            case 2:
-                radiobtn_480.setChecked(true);
-                break;
-            case 3:
-                radiobtn_720.setChecked(true);
-                break;
-            case 4:
-                radiobtn_1080.setChecked(true);
-                break;
-        }
+//        dialogView.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View view) {
+//                onClickListener.onClick(getDialog(), DialogInterface.BUTTON_POSITIVE);
+//                dismiss();
+//                return false;
+//            }
+//        });
 
-        radio_Group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if (i == R.id.quality_atomatic) {
-                    radiobtn_automatic.setChecked(true);
-                    status = 0;
-                    listenerQuality.itemClickToChangeQuality(status);
-                } else if (i == R.id.quality_360) {
-                    radiobtn_360.setChecked(true);
-                    status = 1;
-                    listenerQuality.itemClickToChangeQuality(status);
-                } else if (i == R.id.quality_480) {
-                    radiobtn_480.setChecked(true);
-                    status = 2;
-                    listenerQuality.itemClickToChangeQuality(status);
-                } else if (i == R.id.quality_720) {
-                    radiobtn_720.setChecked(true);
-                    status = 3;
-                    listenerQuality.itemClickToChangeQuality(status);
-                } else if (i == R.id.quality_1080) {
-                    radiobtn_1080.setChecked(true);
-                    status = 4;
-                    listenerQuality.itemClickToChangeQuality(status);
-                }
-                dismiss();
-            }
-        });
+        viewPager.setAdapter(new FragmentAdapter(getChildFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setVisibility(View.GONE);
 
-        AlertDialog alertDialog = builder.create();
-        alertDialog.getWindow().setBackgroundDrawableResource(R.drawable.background_dialog);
-        return alertDialog;
+
+        return dialogView;
     }
 
-    interface ListenerQuality {
-        void itemClickToChangeQuality(int status);
+    public void dismissDialog() {
+        dismiss(); // Dismiss the dialog
+    }
+
+    private static boolean showTabForRenderer(MappedTrackInfo mappedTrackInfo, int rendererIndex) {
+        TrackGroupArray trackGroupArray = mappedTrackInfo.getTrackGroups(rendererIndex);
+        if (trackGroupArray.length == 0) {
+            return false;
+        }
+        int trackType = mappedTrackInfo.getRendererType(rendererIndex);
+        return isSupportedTrackType(trackType);
+    }
+
+    private static boolean isSupportedTrackType(int trackType) {
+        switch (trackType) {
+            case C.TRACK_TYPE_VIDEO:
+            case C.TRACK_TYPE_AUDIO:
+            case C.TRACK_TYPE_TEXT:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private static String getTrackTypeString(Resources resources, int trackType) {
+        switch (trackType) {
+            case C.TRACK_TYPE_VIDEO:
+                return "VIDEO";
+            case C.TRACK_TYPE_AUDIO:
+                return "AUDIO";
+            case C.TRACK_TYPE_TEXT:
+                return "TXT";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private final class FragmentAdapter extends FragmentPagerAdapter {
+
+        public FragmentAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @Override
+        @NonNull
+        public Fragment getItem(int position) {
+            return tabFragments.valueAt(position);
+        }
+
+        @Override
+        public float getPageWidth(int position) {
+            return super.getPageWidth(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getTrackTypeString(getResources(), tabTrackTypes.get(position));
+        }
+    }
+
+    /**
+     * Fragment to show a track selection in tab of the track selection dialog.
+     */
+    public static final class TrackSelectionViewFragment extends Fragment
+            implements TrackSelectionView.TrackSelectionListener {
+        private MappedTrackInfo mappedTrackInfo;
+        private int rendererIndex;
+        private boolean allowAdaptiveSelections;
+        private boolean allowMultipleOverrides;
+        /* package */ boolean isDisabled;
+        /* package */ List<SelectionOverride> overrides;
+
+        public TrackSelectionViewFragment() {
+            // Retain instance across activity re-creation to prevent losing access to init data.
+            setRetainInstance(true);
+        }
+
+        public void init(
+                MappedTrackInfo mappedTrackInfo,
+                int rendererIndex,
+                boolean initialIsDisabled,
+                @Nullable SelectionOverride initialOverride,
+                boolean allowAdaptiveSelections,
+                boolean allowMultipleOverrides) {
+            this.mappedTrackInfo = mappedTrackInfo;
+            this.rendererIndex = rendererIndex;
+            this.isDisabled = initialIsDisabled;
+            this.overrides =
+                    initialOverride == null
+                            ? Collections.emptyList()
+                            : Collections.singletonList(initialOverride);
+            this.allowAdaptiveSelections = allowAdaptiveSelections;
+            this.allowMultipleOverrides = allowMultipleOverrides;
+        }
+
+        @SuppressLint("ResourceAsColor")
+        @Override
+        public View onCreateView(
+                LayoutInflater inflater,
+                @Nullable ViewGroup container,
+                @Nullable Bundle savedInstanceState) {
+            @SuppressLint("PrivateResource")
+            View rootView =
+                    inflater.inflate(
+                            com.google.android.exoplayer2.ui.R.layout.exo_track_selection_dialog, null, /* attachToRoot= */ false);
+            @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+            TrackSelectionView trackSelectionView = rootView.findViewById(R.id.exo_track_selection_view);
+            trackSelectionView.setShowDisableOption(false);
+            trackSelectionView.setAllowMultipleOverrides(false);
+            trackSelectionView.setAllowAdaptiveSelections(false);
+
+            trackSelectionView.setTrackNameProvider(new TrackNameProvider() {
+                @Override
+                public String getTrackName(Format format) {
+                    MainActivity.QualitiesHeight.add(format.height);
+                    MainActivity.QualitiesWidth.add(format.width);
+                    return format.height + "p";
+                }
+            });
+            trackSelectionView.init(
+                    mappedTrackInfo,
+                    rendererIndex,
+                    isDisabled,
+                    overrides,
+                    /* trackFormatComparator= */ null,
+                    /* listener= */ this);
+            return rootView;
+        }
+
+        @Override
+        public void onTrackSelectionChanged(boolean isDisabled, @NonNull List<SelectionOverride> overrides) {
+            this.isDisabled = isDisabled;
+            this.overrides = overrides;
+
+            // Access the parent dialog and dismiss it
+            DialogQuality parentDialog = (DialogQuality) getParentFragment();
+            if (parentDialog != null) {
+                parentDialog.dismissDialog();
+            }
+            //Change quality
+            onClickListener.onClick(DialogQuality.dialogInterface, DialogInterface.BUTTON_POSITIVE);
+            DialogQuality.ItemSelected = true;
+        }
     }
 }
